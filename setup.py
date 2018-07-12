5dddeb13666912d1bc3a2331bd68fb65450cfb7a
Ed Summers <ehs@pobox.com>
2018-07-12 12:18:37 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+if __name__ == "__main__":
+    setup(
+        name='waybackprov',
+        version='0.0.1',
+        url='https://github.com/edsu/waybackprov',
+        author='Ed Summers',
+        author_email='ehs@pobox.com',
+        py_modules=['waybackprov', ],
+        description='Checks the provenance of a URL in the Wayback machine',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        python_requires='>=2.7',
+        entry_points={'console_scripts': ['waybackprov = waybackprov:main']}
+    )
