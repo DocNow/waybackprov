ce8b2bb9997eea47bd7c63574f70a664e026d9fb
Ed Summers <ehs@pobox.com>
2018-07-24 12:38:09 -0400

    new version, borked v0.0.5 upload

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 if __name__ == "__main__":
     setup(
         name='waybackprov',
-        version='0.0.5',
+        version='0.0.6',
         url='https://github.com/edsu/waybackprov',
         author='Ed Summers',
         author_email='ehs@pobox.com',
